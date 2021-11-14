19b9633da9edc355572315748f6bf1c76e223a33
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-11-14 21:29:55 +0530

    learnt new imp functions

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail"
 
-function DetailsPage() {
+function DetailsPage(props) {
     return <>
         <MeetupDetail
             image="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
@@ -9,6 +9,45 @@ function DetailsPage() {
             description="Meetup description"
         />
     </>
+}
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm3'
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    const MeetupId = context.params.meetupId
+    console.log(MeetupId)
+
+    return {
+        props: {
+            meetUpData: {
+                image: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
+                title: "The First Meetup",
+                address: "Some Address",
+                description: "Meetup description"
+            }
+        }
+    }
 }
 
 export default DetailsPage
